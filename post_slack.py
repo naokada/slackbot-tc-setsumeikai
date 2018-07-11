527a86bc05aee08f55b6e4253d43b6f97fde8ba3
Nao Okada <naookada@Naos-MacBook-Pro.local>
2018-07-11 17:42:10 +0900

    add time and text number

--- post_slack.py
+++ post_slack.py
@@ -7,13 +7,32 @@ import requests
 def post_slack():
     
     # slackにポストする形式に整えた文章作成
-    text = '今日も #techcamp-shibuya でリマインドよろしくお願いします。\n以下のフォーマットに担当者のslackと各回のテーブルの番号を入力し、BriefingのGyazoのリンクをその下にコピペして投稿してください。\n```\n@here\n【本日の説明会・体験会について】\n担当: \n来校予定のお客様：以下の写真をご参照ください！\n体験会テーブル：\nお忙しいと思いますが、ご協力お願いします！\n本日もよろしくお願いします！\n```'
+    text = """今日も #techcamp-shibuya でリマインドよろしくお願いします。\n
+        以下のフォーマットに担当者のslackと各回のテーブルの番号を入力し、
+        BriefingのGyazoのリンクをその下にコピペして投稿してください。\n
+        Remindする時間:
+        \t平日: 15:00
+        \t土日祝: 12:00\n
+        ```\n
+        @here
+        \n【本日の説明会・体験会について】\n
+        担当: \n
+        来校予定のお客様：以下の写真をご参照ください！\n
+        体験会テーブル：\n
+        お忙しいと思いますが、ご協力お願いします！\n
+        この投稿とknowledgeを確認後、doneスタンプでリアクションをお願いします。\n
+        本日もよろしくお願いします！\n\n
+        Webサイト制作の体験会のアカウント: ID：html@tech-camp.in (pass:いつもの)\n
+        なお、カリキュラムは以下の通りです。\n
+        https://master.tech-camp.in/curriculums/2981\n
+        https://master.tech-camp.in/curriculums/2982\n
+        ```"""
     post_text('sales_team', text)
 
 def post_text(channel, text):
 
     """
-    Slackの特定のチャンネルにtextを送信する（未検証メソッド）
+    Slackの特定のチャンネルにtextを送信する
     """
     url_slack_api = 'https://slack.com/api/chat.postMessage'
     slack_api_params = {
@@ -26,4 +45,5 @@ def post_text(channel, text):
     }
     requests.post(url_slack_api, data=slack_api_params)
 
-post_slack()
+if __NAME__ == '__main__':
+    post_slack()
