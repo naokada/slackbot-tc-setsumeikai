8a486aae2e6ddf875cf1e3389c5658d8b1bfe1bb
Nao Okada <naookada@Naos-MacBook-Pro.local>
2018-07-17 16:02:26 +0900

    remove tabs and lines

--- post_slack.py
+++ post_slack.py
@@ -7,27 +7,27 @@ import requests
 def post_slack():
     
     # slackにポストする形式に整えた文章作成
-    text = """今日も #techcamp-shibuya でリマインドよろしくお願いします。\n
-        以下のフォーマットに担当者のslackと各回のテーブルの番号を入力し、
-        BriefingのGyazoのリンクをその下にコピペして投稿してください。\n
-        Remindする時間:
-        \t平日: 15:00
-        \t土日祝: 12:00\n
-        ```\n
-        @here
-        \n【本日の説明会・体験会について】\n
-        担当: \n
-        来校予定のお客様：以下の写真をご参照ください！\n
-        体験会テーブル：\n
-        お忙しいと思いますが、ご協力お願いします！\n
-        この投稿とknowledgeを確認後、doneスタンプでリアクションをお願いします。\n
-        本日もよろしくお願いします！\n\n
-        Webサイト制作の体験会のアカウント: ID：html@tech-camp.in (pass:いつもの)\n
-        なお、カリキュラムは以下の通りです。\n
-        https://master.tech-camp.in/curriculums/2981\n
-        https://master.tech-camp.in/curriculums/2982\n
+    text = """今日も #techcamp-shibuya でリマインドよろしくお願いします。
+以下のフォーマットに担当者のslackと各回のテーブルの番号を入力し、
+BriefingのGyazoのリンクをその下にコピペして投稿してください。
+Remindする時間:
+    平日: 15:00
+    土日祝: 12:00
+```
+@here
+【本日の説明会・体験会について】
+担当:
+来校予定のお客様：以下の写真をご参照ください！
+体験会テーブル：
+お忙しいと思いますが、ご協力お願いします！
+この投稿とknowledgeを確認後、doneスタンプでリアクションをお願いします。
+本日もよろしくお願いします！
+Webサイト制作の体験会のアカウント: ID：html@tech-camp.in (pass:いつもの)
+なお、カリキュラムは以下の通りです。
+https://master.tech-camp.in/curriculums/2981
+https://master.tech-camp.in/curriculums/2982
         ```"""
-    post_text('sales_team', text)
+    post_text('test-naokada', text)
 
 def post_text(channel, text):
 
